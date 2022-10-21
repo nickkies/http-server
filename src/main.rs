8f8bbe4ce2f706e5d3b986b5af02f0dc3458320a
YoonSung Kim <opqudk@gmail.com>
2022-10-21 17:36:21 +0900

    feat: "껍데기"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("localhost:8080".to_string());
+    server.run();
 }
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+ */
