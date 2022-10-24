982fb12faea51e369c57df5401689e9b8c02d10d
YoonSung Kim <opqudk@gmail.com>
2022-10-24 10:33:06 +0900

    refactor: "Add mod files"

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,11 @@
+use http::Method;
+use http::Request;
 use server::Server;
-use http::request::Request;
+
+mod http;
+mod server;
 
 fn main() {
     let server = Server::new("localhost:8080".to_string());
     server.run();
 }
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self {
-                addr
-            }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr);
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            // method: super::method::Method,
-            method: Method,
-        }
-    }
-
-    mod method {
-        pub enum Method {
-            GET,
-            POST,
-            PUT,
-            DELETE,
-            HEAD,
-            CONNECT,
-            OPTIONS,
-            TRACE,
-            PATCH,
-        }
-    }
-}
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
- */
