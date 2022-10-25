f5542b7ac2745286a3d6d91410bf68b964d983f3
YoonSung Kim <opqudk@gmail.com>
2022-10-25 15:56:49 +0900

    feat: "Response"

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
-use crate::http::Request;
+use crate::http::{Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 pub struct Server {
@@ -30,6 +30,8 @@ impl Server {
                             match Request::try_from(&buffer[..]) {
                                 Ok(request) => {
                                     dbg!(request);
+                                    let response = Response::new(StatusCode::Ok, Some("<h1>Hello world!!</h1>".to_string()));
+                                    write!(stream, "{}", response);
                                 },
                                 Err(e) => println!("Failed to parse a request: {}", e),
                             }
